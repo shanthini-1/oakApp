eb91dfb04f498d281cd5d3576509d1b2964606d5
shanthini-1 <shanthini.vibutheesh@chainsys.com>
2022-07-25 11:13:33 +0530

    included MVC controller and view

---
+++ src/main/java/com/chainsys/oakApp/MainApp.java
@@ -0,0 +1,10 @@
+package com.chainsys.oakApp;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class MainApp {
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
